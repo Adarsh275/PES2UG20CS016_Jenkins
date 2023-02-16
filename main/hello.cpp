5924cdbb01fa84f144f25ae7fe31ade1e7fcd161
Adarsh275 <adarshaman2000@gmail.com>
2023-02-16 21:40:20 +0530

    Adding Hello world and Dockerfile

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My Name is Adarsh Kumar\n";
+  cout << "My SRN NO: PES2UG20CS016 \n"
+  cout << "I have successfully built and run\n";
+  return 0;
+}
