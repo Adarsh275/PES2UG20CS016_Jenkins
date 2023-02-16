e241166ca3c36952bba2b56bd47a95f773edaa3c
Adarsh Kumar <93258651+Adarsh275@users.noreply.github.com>
2023-02-16 22:16:54 +0530

    Bug fixed.

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "My Name is Adarsh Kumar\n";
-  cout << "My SRN NO: PES2UG20CS016 \n"
+  cout << "My SRN NO: PES2UG20CS016 \n";
   cout << "I have successfully built and run\n";
   return 0;
 }
