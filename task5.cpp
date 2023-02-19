3253f82d0da06b466499acd10d03328edb167109
Adarsh Kumar <93258651+Adarsh275@users.noreply.github.com>
2023-02-19 13:04:33 +0530

    Jenkins task 5 code for hello world

---
+++ task5.cpp
@@ -0,0 +1,13 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My Name is Adarsh Kumar\n";
+  cout << "My SRN NO: PES2UG20CS016 \n";
+  cout << "I Have modified the code and testing it for the 4th task\n";
+  
+  return 0;
+}
