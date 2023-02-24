cb079d3ff9679b014e16978906e91dd30649a07e
Adarsh275 <adarshaman2000@gmail.com>
2023-02-24 14:17:13 +0530

    Added task 5 program

---
+++ task5/task5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My Name is Adarsh Kumar\n";
+  cout << "My SRN NO: PES2UG20CS016 \n";
+  cout << "This is my Task5 cpp file";
+  
+  return 0;
+}
